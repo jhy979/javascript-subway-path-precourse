3067bec11a6d62edc25951e6d89544001911ff19
jhy979 <jhy979@gmail.com>
2021-12-17 19:05:49 +0900

    refactor: radio input 선택에 따라 초기 설정 진행 변경

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import View from "./view/view.js";
+import SubwayMap from "./model/subwayMap.js";
 
 new View();
+const subwayMap = new SubwayMap();
+subwayMap.getShortestPath("매봉", "양재");
--- src/model/subwayMap.js
+++ src/model/subwayMap.js
@@ -5,13 +5,23 @@ export default class SubwayMap {
   constructor() {
     this.stationInfo = new Set();
     this.dijkstra = new Dijkstra();
-    this.makeDefaultMap();
+    this.makeDefaultMap("dist");
   }
 
-  makeDefaultMap() {
+  makeDefaultMap(type) {
+    if (type === "dist") {
+      defaultMap.forEach((info) => {
+        this.dijkstra.addEdge(info.v1, info.v2, info.dist);
+      });
+      return;
+    }
+
     defaultMap.forEach((info) => {
-      this.dijkstra.addEdge(info.v1, info.v2, info.dist);
       this.dijkstra.addEdge(info.v1, info.v2, info.time);
     });
   }
+
+  // getShortestPath(depart, dest) {
+  //   console.log(this.dijkstra.findShortestPath("매봉", "교대"));
+  // }
 }
