da36bbf7c2280b313dcd9be21ede46e691dcc929
jhy979 <jhy979@gmail.com>
2021-12-17 21:13:23 +0900

    refactor(subwayMap): shortest path 반환 시 거리와 시간 반환

--- src/model/subwayMap.js
+++ src/model/subwayMap.js
@@ -22,6 +22,24 @@ export default class SubwayMap {
   }
 
   getShortestPath(depart, dest) {
-    return this.dijkstra.findShortestPath(depart, dest);
+    const result = this.dijkstra.findShortestPath(depart, dest);
+    let dist = 0;
+    let time = 0;
+    for (let i = 0; i < result.length - 1; i += 1) {
+      for (let j = 0; j < defaultMap.length; j += 1) {
+        if (
+          defaultMap[j].v1 === result[i] &&
+          defaultMap[j].v2 === result[i + 1]
+        ) {
+          dist += defaultMap[j].dist;
+          time += defaultMap[j].time;
+          break;
+        }
+      }
+    }
+    result.push(dist);
+    result.push(time);
+
+    return result;
   }
 }
